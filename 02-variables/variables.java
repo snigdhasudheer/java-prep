822a1b445fad09b301169d1742afa072e330b3be
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-02 11:12:26 +0530

    Create variables.java

---
+++ 02-variables/variables.java
@@ -0,0 +1,29 @@
+// Day 2
+// Topic: Variables & Data Types
+
+public class VariablesExample {
+    public static void main(String[] args) {
+        
+        // Integer
+        int age = 20;
+
+        // Decimal number
+        double height = 5.7;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean
+        boolean isStudent = true;
+
+        // String (text)
+        String name = "Snigdha";
+
+        // Printing values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
