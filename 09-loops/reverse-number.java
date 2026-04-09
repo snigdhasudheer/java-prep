afef32e8ff362a3a7afd1baa19f13fc39f78fb47
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-09 10:57:21 +0530

    Add ReverseNumber program to reverse an integer

---
+++ 09-loops/reverse-number.java
@@ -0,0 +1,25 @@
+// Reverse a number
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed: " + reverse);
+
+        sc.close();
+    }
+}
