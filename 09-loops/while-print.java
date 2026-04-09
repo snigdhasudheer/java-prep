cb9dd4a239bb0126e3fda96abd7837f40b6d6b2e
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-09 10:56:17 +0530

    Day 9: While and Do-While loops (print, sum, reverse)

---
+++ 09-loops/while-print.java
@@ -0,0 +1,22 @@
+// Print numbers from 1 to N using while
+
+import java.util.Scanner;
+
+public class WhilePrint {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+
+        sc.close();
+    }
+}
