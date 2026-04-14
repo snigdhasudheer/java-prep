c9ce9d0abbf0f64791ef254c005abe002add2387
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-14 14:50:23 +0530

    Inverted Pyramid

---
+++ 12-patterns/inverted-pyramid.java
@@ -0,0 +1,23 @@
+// Inverted pyramid
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = n; i >= 1; i--) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
