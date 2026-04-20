03366ad36ce3240fa9bcd80a03d70daca656a41f
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-20 18:44:21 +0530

    EvenOdd

---
+++ 15-functions/evenOdd.java
@@ -0,0 +1,15 @@
+// Check even or odd using function
+
+public class EvenOdd {
+
+    static void check(int num) {
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+
+    public static void main(String[] args) {
+        check(7);
+    }
+}
