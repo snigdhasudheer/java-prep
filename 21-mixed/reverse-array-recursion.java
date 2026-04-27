d4fbf4bb4021216404c960e1a395e0040bd1548c
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-04-27 21:42:51 +0530

    Reverse array using recursion

---
+++ 21-mixed/reverse-array-recursion.java
@@ -0,0 +1,24 @@
+public class ReverseRec {
+
+    static void reverse(int[] arr, int start, int end) {
+        if (start >= end) return;
+
+        // swap
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        reverse(arr, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        reverse(arr, 0, arr.length - 1);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
