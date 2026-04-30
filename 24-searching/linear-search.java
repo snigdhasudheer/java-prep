245cdd50dfe1c218c954d018bc98b5b4e5cb3e4f
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-05-01 00:03:11 +0530

    Linear Search

---
+++ 24-searching/linear-search.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Not Found");
+        }
+    }
+}
