60e4b89a45bd646ae83eb12bae9d457268f40465
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-05-10 20:18:23 +0530

    Find duplicate elements

---
+++ 28-problem-solving/duplicate.java
@@ -0,0 +1,18 @@
+public class Duplicate {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 4, 1};
+
+        System.out.println("Duplicate Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
