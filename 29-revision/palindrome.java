06cb44d99a101322dfff5e2036fd10d9bfb835ce
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-05-11 15:55:43 +0530

    Create palindrome.java

---
+++ 29-revision/palindrome.java
@@ -0,0 +1,23 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+
+        int num = 121;
+        int original = num;
+
+        int reverse = 0;
+
+        while (num != 0) {
+
+            int digit = num % 10;
+
+            reverse = reverse * 10 + digit;
+
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
