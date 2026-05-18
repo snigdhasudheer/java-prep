81f6e1283de52ee0e85649d4544b8a0b578fce19
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-05-18 21:34:17 +0530

    Frequency count using HashMap

---
+++ 31-hashmap/frequency-count.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 2, 3, 1, 4, 2};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        System.out.println(map);
+    }
+}
