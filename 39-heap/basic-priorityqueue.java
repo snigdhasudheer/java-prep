e058c4fa915131570147150b5bcd72df1e362783
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-05-31 09:46:19 +0530

    Create basic-priorityqueue.java

---
+++ 39-heap/basic-priorityqueue.java
@@ -0,0 +1,23 @@
+// Basic Priority Queue
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq =
+                new PriorityQueue<>();
+
+        pq.add(40);
+        pq.add(10);
+        pq.add(30);
+        pq.add(20);
+
+        System.out.println(pq);
+
+        System.out.println(pq.poll());
+
+        System.out.println(pq.peek());
+    }
+}
