6eac30c19401f6a0c7d4ea1627413b59af5f1631
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-06-14 10:00:39 +0530

    output = 5

---
+++ 49-greedy/value-weight-ratio.java
@@ -0,0 +1,13 @@
+public class RatioDemo {
+
+    public static void main(String[] args) {
+
+        int value = 100;
+        int weight = 20;
+
+        double ratio =
+                (double)value / weight;
+
+        System.out.println(ratio);
+    }
+}
