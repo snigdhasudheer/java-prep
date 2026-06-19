441e8cfeb932bb6977e1921c2e9b3f650e79d6b2
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-06-19 15:01:28 +0530

    Create climbing-stairs.java

---
+++ 53-dynamic-programming/climbing-stairs.java
@@ -0,0 +1,26 @@
+public class ClimbingStairs {
+
+    static int climb(int n) {
+
+        if(n <= 2)
+            return n;
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<=n;i++) {
+
+            dp[i] =
+                dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(climb(5));
+    }
+}
