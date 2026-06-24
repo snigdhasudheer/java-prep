d34e671fe9b55e23e0fe817cd118350adba96a4e
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-06-24 21:14:59 +0530

    Create binary-strings.java

---
+++ 58-backtracking/binary-strings.java
@@ -0,0 +1,22 @@
+public class BinaryStrings {
+
+    static void generate(String str, int n){
+
+        if(str.length() == n){
+
+            System.out.println(str);
+
+            return;
+        }
+
+        generate(str + "0", n);
+
+        generate(str + "1", n);
+    }
+
+    public static void main(String[] args){
+
+        generate("",3);
+
+    }
+}
