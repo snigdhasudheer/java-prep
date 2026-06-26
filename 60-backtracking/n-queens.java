8f7155f66f2eb8c88b450bbc65c5db89cf02e999
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-06-26 20:56:55 +0530

    Create n-queens.java

---
+++ 60-backtracking/n-queens.java
@@ -0,0 +1,29 @@
+public class NQueens {
+
+    static boolean isSafe(){
+
+        // Check column
+
+        // Check left diagonal
+
+        // Check right diagonal
+
+        return true;
+    }
+
+    static void solve(){
+
+        // Place queen
+
+        // Recursive call
+
+        // Remove queen (Backtrack)
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(
+                "N-Queens Skeleton");
+
+    }
+}
