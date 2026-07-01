30d8871b1a53de4615479a260399e35be6d2bf46
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-01 09:23:28 +0530

    Create max-sum-subarray.java

---
+++ 64-techniques/max-sum-subarray.java
@@ -0,0 +1,30 @@
+public class SlidingWindow {
+
+    public static void main(String[] args){
+
+        int[] arr = {2,1,5,1,3,2};
+
+        int k = 3;
+
+        int windowSum = 0;
+
+        for(int i=0;i<k;i++)
+            windowSum += arr[i];
+
+        int maxSum = windowSum;
+
+        for(int i=k;i<arr.length;i++){
+
+            windowSum =
+                windowSum
+                - arr[i-k]
+                + arr[i];
+
+            maxSum =
+                Math.max(maxSum,
+                         windowSum);
+        }
+
+        System.out.println(maxSum);
+    }
+}
