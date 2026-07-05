0a05944f26932fbcce10da82a063c7c7f3dd444d
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-05 21:03:04 +0530

    HashSet

---
+++ 65-hashing/hashset-demo.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+    public static void main(String[] args){
+
+        HashSet<Integer> set =
+                new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(10);
+        set.add(30);
+
+        System.out.println(set);
+
+        System.out.println(
+                set.contains(20));
+
+        set.remove(10);
+
+        System.out.println(set);
+    }
+}
