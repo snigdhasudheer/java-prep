1119d920a2070d4ea312ffaf1e32cf343707db0a
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-12 20:54:31 +0530

    Create reverse-words.java

---
+++ 66-string-algorithms/reverse-words.java
@@ -0,0 +1,19 @@
+public class ReverseWords {
+
+    public static void main(String[] args){
+
+        String s =
+                "I Love Java";
+
+        String[] words =
+                s.split(" ");
+
+        for(int i=
+            words.length-1;
+            i>=0;
+            i--)
+
+            System.out.print(
+                    words[i]+" ");
+    }
+}
