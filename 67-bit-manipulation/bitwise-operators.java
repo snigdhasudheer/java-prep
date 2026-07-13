6e5910a6e8ce86a7a424d39ce04cbe327986e0cb
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-13 09:40:50 +0530

    Create bitwise-operators.java

---
+++ 67-bit-manipulation/bitwise-operators.java
@@ -0,0 +1,14 @@
+public class BitwiseOperators {
+
+    public static void main(String[] args){
+
+        int a = 5;   //0101
+        int b = 3;   //0011
+
+        System.out.println("AND : " + (a & b));
+
+        System.out.println("OR  : " + (a | b));
+
+        System.out.println("XOR : " + (a ^ b));
+    }
+}
