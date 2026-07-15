1b6b635dcbdce2e027a853bde8e2148a8e268ad6
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-15 09:35:50 +0530

    Add CountBits class to count set bits in an integer

---
+++ 67-bit-manipulation/count-set-bits.java
@@ -0,0 +1,18 @@
+public class CountBits {
+
+    public static void main(String[] args){
+
+        int n = 13;
+
+        int count = 0;
+
+        while(n > 0){
+
+            count += (n & 1);
+
+            n >>= 1;
+        }
+
+        System.out.println(count);
+    }
+}
