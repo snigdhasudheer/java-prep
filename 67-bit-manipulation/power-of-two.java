5a62d3effcfc819e5388330b7b458ef119288dac
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-16 09:46:29 +0530

    Create power-of-two.java

---
+++ 67-bit-manipulation/power-of-two.java
@@ -0,0 +1,16 @@
+public class PowerOfTwo {
+
+    public static void main(String[] args){
+
+        int n = 16;
+
+        if(n > 0 &&
+           (n & (n - 1)) == 0)
+
+            System.out.println("Power of Two");
+
+        else
+
+            System.out.println("Not Power of Two");
+    }
+}
