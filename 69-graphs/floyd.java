be54b2d2cb551d0145859a586d4a2a185835edfd
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-07-24 10:06:52 +0530

    Create floyd.java

---
+++ 69-graphs/floyd.java
@@ -0,0 +1,7 @@
+for(int k=0;k<n;k++)
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            dist[i][j] =
+                Math.min(
+                    dist[i][j],
+                    dist[i][k]+dist[k][j]);
