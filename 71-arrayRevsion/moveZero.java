c102eebd69a25c03d760443f035f13e5531d2180
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-08-01 20:07:12 +0530

    movezeros

---
+++ 71-arrayRevsion/moveZero.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public static void main(String[] args){
+
+        int[] arr = {0,1,0,3,12};
+
+        int index = 0;
+
+        for(int num : arr){
+            if(num != 0)
+                arr[index++] = num;
+        }
+
+        while(index < arr.length){
+            arr[index++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
