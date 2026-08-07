8fc7d8f3ff4ed16c08db2eea3e0c767929799bbe
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-08-07 21:26:54 +0530

    group anagram revision

---
+++ 72-pattern/groupAnagram.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+
+        System.out.println(map.values());
+    }
+}
