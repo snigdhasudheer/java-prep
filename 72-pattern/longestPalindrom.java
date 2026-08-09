6416cecf08687a006ddbbc7bd1226c6bb25f0615
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-08-09 18:54:04 +0530

    Longest Palindromic Substring

---
+++ 72-pattern/longestPalindrom.java
@@ -0,0 +1,36 @@
+public class LongestPalindrome {
+
+    static String expand(String s, int left, int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "babad";
+
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            String odd = expand(s, i, i);
+            String even = expand(s, i, i + 1);
+
+            if (odd.length() > ans.length())
+                ans = odd;
+
+            if (even.length() > ans.length())
+                ans = even;
+        }
+
+        System.out.println(ans);
+    }
+}
