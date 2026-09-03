770a974754c5783a10ea0aa970c039ca2e61dcb0
snigdhasudheer <143707252+snigdhasudheer@users.noreply.github.com>
2026-09-03 11:54:36 +0530

    BinarySearch

---
+++ 75-Binary Search + Searching Interview Problems/BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+
+    static int search(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9, 11};
+
+        System.out.println(search(arr, 7));
+    }
+}
